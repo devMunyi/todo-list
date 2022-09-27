0740330f61a5f87720a1cc3fb7316f5687545f40
devMunyi <samunyi90@gmail.com>
2022-09-27 19:33:21 +0300

    fix linters error

--- modules/displayToDOList.js
+++ modules/displayToDOList.js
@@ -6,8 +6,8 @@ const displayToDOList = (todoList) => {
       }">
     <div class="tdl-description__left--item">
       <input type="checkbox" id="myCheck-${item.index}" ${
-        item.completed ? 'checked' : ''
-      }/>
+  item.completed ? 'checked' : ''
+}/>
       <label for="myCheck">${item.description}</label>
     </div>
     <div class="tdl-description__right--item right--item">
@@ -15,7 +15,7 @@ const displayToDOList = (todoList) => {
       <div class="dot"></div>
       <div class="dot"></div>
     </div>
-  </div>`
+  </div>`,
     )
     .join('');
 
